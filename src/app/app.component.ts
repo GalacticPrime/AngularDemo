d8c5916703d80ccdb2611aa80ad37415f57a3244
GalacticPrime <primesh1427@gmail.com>
2021-01-10 16:52:18 +0530

    Pushed everything at once

---
+++ src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { ArgumentOutOfRangeError } from 'rxjs';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styles: [`
+    h2{
+      color: orange;
+    }
+  `]
+  //styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+
+}
